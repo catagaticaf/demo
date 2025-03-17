ab599990a738fdb7bdfb3bd5d30e5dff1db65595
catagaticaf <cata.gatica.f@hotmail.com.ar>
2025-03-17 13:21:18 -0300

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import string
+import random
 print ("probando!!")
+print ("generador de claves")
+chars = string.ascii_letters + string.digits + string.punctuation
+password = ""
+length = 10
+
+for c in range(length):
+    password = password + random.choice(chars)
+
+print ("contraseña generada: " + password)
