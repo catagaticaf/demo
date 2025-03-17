a0197ffc753b81aa5e754d647aaeb76b3f86b4ed
catagaticaf <cata.gatica.f@hotmail.com.ar>
2025-03-17 13:32:10 -0300

    modificacion

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ print ("probando!!")
 print ("generador de claves")
 chars = string.ascii_letters + string.digits + string.punctuation
 password = ""
-length = 10
+length = int(input("ingrese la longitud de la contraseña: "))
 
 for c in range(length):
     password = password + random.choice(chars)
